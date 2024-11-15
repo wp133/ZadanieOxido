710498cc4f9d188e2af5647094950805207dd6a4
wp133 <131851168+wp133@users.noreply.github.com>
2024-11-15 23:37:03 +0100

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,52 @@
+import tkinter as tk
+from tkinter import filedialog
+from formatting import article_formatting
+from tkhtmlview import HTMLLabel
+
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("text files", "Zadanie dla JJunior AI Developera - tresc artykulu.txt")])
+    if file_path:
+        print(file_path)
+        desc = article_formatting(file_path)
+        file1 = open("artykul.html", "w")
+        file1.write(desc)
+        #file1.close()
+
+        file2 = open("showcase.html", "a")
+        file2.write(("<body>\n"))
+        file2.write(desc)
+        file2.write(("\n</body>"))
+        #file2.close()
+
+def load_html_file(showfile_path):
+    with open(showfile_path, 'r') as file:
+        return file.read()
+
+def reset():
+    file1 = open("showcase.html", "w")
+    file1.write("")
+
+root = tk.Tk()
+root.title("Article Generator")
+root.geometry("800x600")
+
+
+open_button = tk.Button(root, text="Open Text File", command=open_file)
+open_button.pack(pady=20)
+
+open_button = tk.Button(root, text="Reset", command=reset)
+open_button.pack(pady=20)
+
+html_content = load_html_file("showcase.html")
+html_label = HTMLLabel(root, html=html_content)
+html_label.pack(fill="both", expand=True)
+
+root.mainloop()
+
+
+
+
+
+#result_label.config(text=desc)
+#result_label = tk.Label(root, text="", wraplength=380, justify="left")
+#result_label.pack(pady=20)
---
+++ readme.txt
@@ -0,0 +1,12 @@
+Asystent HTML - aplikacja strukturyzująca tekst zadany w pliku txt i tworząca na jego podstawie pliki: artykul.html (bez sekcji <body>) oraz showcase.html (z sekcją <body>)
+
+Asystenta można otworzyć poprzez dwukrotne kliknięcie załączonego pliku wykonywalnego "gui.exe". Deweloper uprzedza, że istnieje niezerowa szansa, że plik ów nie zadziała.
+Wówczas zaleca się skompilowanie pliku "gui.py".
+
+Aplikacja posiada własny interefjs graficzny. Przyciskiem "Open text file" można wybrać dowolny plik tekstowy na podstawie którego chcemy stworzyć artykuł. 
+Domyślnie wybór proponowanych plików jest zawężony do pliku "Zadanie dla JJunior AI Developera - tresc artykulu" ale można łatwo zamiast tego otworzyć dowolny plik txt zaminiając nazwę pliku na "*".
+
+Przycisk Reset służy do czyszczenia pliku showcase.html. Aplikacja domyślnie pokazuje ostatni otwarty artykuł. Tak, to feature.
+
+#####################
+Załączone są tez pliki "requirements.txt" zawierający listę używanych bibliotek, oraz "image_placeholder.jpg" będący imponującym fajerwerkiem graficznym.
