1acc74830f97aa6fdfaaa0a3934cca1874a69a63
wp133 <131851168+wp133@users.noreply.github.com>
2024-11-16 06:01:35 +0100

    Update generating.py

--- Zadanie_extra/generating.py
+++ Zadanie_extra/generating.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 
 def article_generating():
-    API_KEY = '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'
+    API_KEY = 'tu umiesc klucz'
     try:
         client = OpenAI(api_key=API_KEY)
 
